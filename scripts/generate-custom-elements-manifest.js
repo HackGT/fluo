1c3bf5a09565e60bf4254b394cffbf9c5e1577e5
Elton <eltonp3103@gmail.com>
2020-12-16 03:43:19 -0500

    fix generate-custom-elements-manifest file

--- scripts/generate-custom-elements-manifest.js
+++ scripts/generate-custom-elements-manifest.js
@@ -1,31 +1,22 @@
 import glob from "glob";
 import path from "path";
 import fs from "fs";
+import {tsImport} from "ts-import";
 
-const defLocs = glob.sync(
-    path.resolve(__dirname, "../.temp/**/*.definition.js")
-);
 const destLocs = glob
     .sync(path.resolve(__dirname, "../src/**/*.definition.ts"))
-    .map(path.dirname);
 
-const locations = zip(defLocs, destLocs);
-
-locations.forEach(([def, dest]) => {
-    import(def).then(data => {
-        const contents = JSON.stringify(data.default, null, 4);
-        const destPath = path.join(dest, "custom-elements.json");
-        fs.writeFile(destPath, contents, e => {
-            if (e) {
-                console.log(e);
-                process.exit();
-            } else {
-                console.log(`wrote to ${destPath}`);
-            }
-        });
+destLocs.forEach(async (dest) => {
+    const data = await tsImport.compile(dest);
+    const contents = JSON.stringify(data.default, null, 4);
+    const destPath = path.join(path.dirname(dest), "custom-elements.json");
+    fs.writeFile(destPath, contents, e => {
+        if (e) {
+            console.log(e);
+            process.exit();
+        } else {
+            console.log(`wrote to ${destPath}`);
+        }
     });
 });
 
-function zip(a, b) {
-    return a.map((k, i) => [k, b[i]]);
-}
