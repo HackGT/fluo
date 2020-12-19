d84deefe0fc6c2bbb48805c0eba9051cbe7a676a
Ayush Goyal <agoyal2001@gmail.com>
2020-12-18 22:52:01 -0500

    Fix styling and add testing to pre commit

--- src/components/button/test/button.spec.tsx
+++ src/components/button/test/button.spec.tsx
@@ -2,17 +2,17 @@ import { newSpecPage } from "@stencil/core/testing";
 import { Button } from "../button";
 
 describe("fl-button", () => {
-    it("renders", async () => {
-        const page = await newSpecPage({
-            components: [Button],
-            html: "<fl-button></fl-button>",
-        });
-        expect(page.root).toEqualHtml(`
+  it("renders", async () => {
+    const page = await newSpecPage({
+      components: [Button],
+      html: "<fl-button></fl-button>",
+    });
+    expect(page.root).toEqualHtml(`
       <fl-button>
         <mock:shadow-root>
           <slot></slot>
         </mock:shadow-root>
       </fl-button>
     `);
-    });
+  });
 });
