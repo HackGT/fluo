6ab90d2312da5155345479d908234249a1eb5eba
chtc06 <chtc06@github.com>
2021-02-11 17:58:50 -0500

    changed alert

--- packages/core/src/components/alert/alert.tsx
+++ packages/core/src/components/alert/alert.tsx
@@ -1,20 +1,5 @@
 import { Component, Element, Event, EventEmitter, Method, Prop, State, Watch, h } from '@stencil/core';
 
-const toastStack = Object.assign(document.createElement('div'), { className: 'fl-toast-stack' });
-
-/**
- * @since 2.0
- * @status stable
- *
- * @slot - The alert's content.
- * @slot icon - An icon to show in the alert.
- *
- * @part base - The component's base wrapper.
- * @part icon - The container that wraps the alert icon.
- * @part message - The alert message.
- * @part close-button - The close button.
- */
-
 @Component({
   tag: 'fl-alert',
   styleUrl: 'alert.scss',
@@ -28,67 +13,20 @@ export class Alert {
 
   @State() isVisible = false;
 
-  /** Indicates whether or not the alert is open. You can use this in lieu of the show/hide methods. */
   @Prop({ mutable: true, reflect: true }) open = false;
 
-  /** Set to true to make the alert closable. */
-  @Prop({ reflect: true }) closable = false;
+  @Prop({ reflect: true }) closable = true;
 
-  /** The type of alert. */
-  @Prop({ reflect: true }) type: 'primary' | 'success' | 'info' | 'warning' | 'danger' = 'primary';
-
-  /**
-   * The length of time, in milliseconds, the alert will show before closing itself. If the user interacts with the
-   * alert before it closes (e.g. moves the mouse over it), the timer will restart.
-   */
-  @Prop() duration = Infinity;
+  @Prop({ reflect: true }) type: 'primary';
 
   @Watch('open')
   handleOpenChange() {
     this.open ? this.show() : this.hide();
   }
 
-  @Watch('duration')
-  handleDurationChange() {
-    this.restartAutoHide();
-  }
-
-  /** Emitted when the alert opens. Calling `event.preventDefault()` will prevent it from being opened. */
-  @Event({ eventName: 'fl-show' }) slShow: EventEmitter;
-
-  /** Emitted after the alert opens and all transitions are complete. */
-  @Event({ eventName: 'fl-after-show' }) slAfterShow: EventEmitter;
-
-  /** Emitted when the alert closes. Calling `event.preventDefault()` will prevent it from being closed. */
-  @Event({ eventName: 'fl-hide' }) slHide: EventEmitter;
-
-  /** Emitted after the alert closes and all transitions are complete. */
-  @Event({ eventName: 'fl-after-hide' }) slAfterHide: EventEmitter;
-
-  connectedCallback() {
-    this.handleCloseClick = this.handleCloseClick.bind(this);
-    this.handleMouseMove = this.handleMouseMove.bind(this);
-    this.handleTransitionEnd = this.handleTransitionEnd.bind(this);
-  }
-
-  componentWillLoad() {
-    // Show on init if open
-    if (this.open) {
-      this.show();
-    }
-  }
-
-  /** Shows the alert. */
   @Method()
   async show() {
-    // Prevent subsequent calls to the method, whether manually or triggered by the `open` watcher
     if (this.isVisible) {
-      return;
-    }
-
-    const slShow = this.slShow.emit();
-    if (slShow.defaultPrevented) {
-      this.open = false;
       return;
     }
 
@@ -100,54 +38,15 @@ export class Alert {
     }
   }
 
-  /** Hides the alert */
   @Method()
   async hide() {
-    // Prevent subsequent calls to the method, whether manually or triggered by the `open` watcher
     if (!this.isVisible) {
-      return;
-    }
-
-    const slHide = this.slHide.emit();
-    if (slHide.defaultPrevented) {
-      this.open = true;
       return;
     }
 
     clearTimeout(this.autoHideTimeout);
     this.isVisible = false;
     this.open = false;
-  }
-
-  /**
-   * Displays the alert as a toast notification. This will move the alert out of its position in the DOM and, when
-   * dismissed, it will be removed from the DOM completely. By storing a reference to the alert, you can reuse it by
-   * calling this method again. The returned promise will resolve after the alert is hidden.
-   */
-  @Method()
-  async toast() {
-    return new Promise<void>(resolve => {
-      if (!toastStack.parentElement) {
-        document.body.append(toastStack);
-      }
-
-      toastStack.append(this.host);
-      requestAnimationFrame(() => this.show());
-
-      this.host.addEventListener(
-        'fl-after-hide',
-        () => {
-          this.host.remove();
-          resolve();
-
-          // Remove the toast stack from the DOM when there are no more alerts
-          if (toastStack.querySelector('fl-alert') === null) {
-            toastStack.remove();
-          }
-        },
-        { once: true }
-      );
-    });
   }
 
   handleCloseClick() {
@@ -161,7 +60,6 @@ export class Alert {
   handleTransitionEnd(event: TransitionEvent) {
     const target = event.target as HTMLElement;
 
-    // Ensure we only emit one event when the target element is no longer visible
     if (event.propertyName === 'opacity' && target.classList.contains('alert')) {
       this.isVisible = this.open;
       this.open ? this.slAfterShow.emit() : this.slAfterHide.emit();
@@ -186,10 +84,6 @@ export class Alert {
           'alert--visible': this.isVisible,
           'alert--closable': this.closable,
           'alert--primary': this.type === 'primary',
-          'alert--success': this.type === 'success',
-          'alert--info': this.type === 'info',
-          'alert--warning': this.type === 'warning',
-          'alert--danger': this.type === 'danger'
         }}
         role="alert"
         aria-live="assertive"
@@ -208,7 +102,7 @@ export class Alert {
 
         {this.closable && (
           <span class="alert__close">
-            <sl-icon-button exportparts="base:close-button" name="x" onClick={this.handleCloseClick} />
+            {/* Create close icon component!! */}
           </span>
         )}
       </div>
