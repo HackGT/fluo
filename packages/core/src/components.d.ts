122fe31aa87a8db27273002a6bcc5a49c2d345bb
Edmund Xin <mxinburritos@gmail.com>
2021-05-29 12:25:14 -0600

    fix expand animation

--- packages/core/src/components.d.ts
+++ packages/core/src/components.d.ts
@@ -5,9 +5,24 @@
  * It contains typing information for all components that exist in this project.
  */
 import { HTMLStencilElement, JSXBase } from "@stencil/core/internal";
+import { atypes } from "./components/alert/atypes";
 import { btypes, positions } from "./components/badge/btypes";
 import { Rule } from "./utils/utils";
 export namespace Components {
+    interface FlAlert {
+        "body"?: string;
+        "closed"?: boolean;
+        "fixed": boolean;
+        "heading"?: string;
+        /**
+          * Set to true to draw the link in a loading state.
+         */
+        "loading": boolean;
+        /**
+          * The link's type
+         */
+        "type": atypes;
+    }
     interface FlBadge {
         "pill": boolean;
         "position": positions;
@@ -172,6 +187,12 @@ export namespace Components {
     }
 }
 declare global {
+    interface HTMLFlAlertElement extends Components.FlAlert, HTMLStencilElement {
+    }
+    var HTMLFlAlertElement: {
+        prototype: HTMLFlAlertElement;
+        new (): HTMLFlAlertElement;
+    };
     interface HTMLFlBadgeElement extends Components.FlBadge, HTMLStencilElement {
     }
     var HTMLFlBadgeElement: {
@@ -227,6 +248,7 @@ declare global {
         new (): HTMLFlSwitchElement;
     };
     interface HTMLElementTagNameMap {
+        "fl-alert": HTMLFlAlertElement;
         "fl-badge": HTMLFlBadgeElement;
         "fl-button": HTMLFlButtonElement;
         "fl-checkbox": HTMLFlCheckboxElement;
@@ -239,6 +261,20 @@ declare global {
     }
 }
 declare namespace LocalJSX {
+    interface FlAlert {
+        "body"?: string;
+        "closed"?: boolean;
+        "fixed"?: boolean;
+        "heading"?: string;
+        /**
+          * Set to true to draw the link in a loading state.
+         */
+        "loading"?: boolean;
+        /**
+          * The link's type
+         */
+        "type"?: atypes;
+    }
     interface FlBadge {
         "pill"?: boolean;
         "position"?: positions;
@@ -406,6 +442,7 @@ declare namespace LocalJSX {
         "value"?: string;
     }
     interface IntrinsicElements {
+        "fl-alert": FlAlert;
         "fl-badge": FlBadge;
         "fl-button": FlButton;
         "fl-checkbox": FlCheckbox;
@@ -421,6 +458,7 @@ export { LocalJSX as JSX };
 declare module "@stencil/core" {
     export namespace JSX {
         interface IntrinsicElements {
+            "fl-alert": LocalJSX.FlAlert & JSXBase.HTMLAttributes<HTMLFlAlertElement>;
             "fl-badge": LocalJSX.FlBadge & JSXBase.HTMLAttributes<HTMLFlBadgeElement>;
             "fl-button": LocalJSX.FlButton & JSXBase.HTMLAttributes<HTMLFlButtonElement>;
             "fl-checkbox": LocalJSX.FlCheckbox & JSXBase.HTMLAttributes<HTMLFlCheckboxElement>;
